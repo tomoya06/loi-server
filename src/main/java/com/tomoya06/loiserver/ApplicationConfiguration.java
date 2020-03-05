ec4ed00b76e3c84cfdc22c79f7f83dbda57b52b1
tomoya00 <peng4766@gmail.com>
2020-03-05 15:42:32 +0800

    cors

--- src/main/java/com/tomoya06/loiserver/ApplicationConfiguration.java
+++ src/main/java/com/tomoya06/loiserver/ApplicationConfiguration.java
@@ -9,6 +9,9 @@ import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -27,5 +30,26 @@ public class ApplicationConfiguration {
     converter.setTypeMapper(new DefaultMongoTypeMapper(null));
 
     return converter;
+  }
+
+  @Bean
+  public CorsFilter corsFilter() {
+    //1.添加CORS配置信息
+    CorsConfiguration config = new CorsConfiguration();
+    //放行哪些原始域
+    config.addAllowedOrigin("*");
+    //是否发送Cookie信息
+    config.setAllowCredentials(true);
+    //放行哪些原始域(请求方式)
+    config.addAllowedMethod("*");
+    //放行哪些原始域(头部信息)
+    config.addAllowedHeader("*");
+
+    //2.添加映射路径
+    UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+    configSource.registerCorsConfiguration("/**", config);
+
+    //3.返回新的CorsFilter.
+    return new CorsFilter(configSource);
   }
 }
